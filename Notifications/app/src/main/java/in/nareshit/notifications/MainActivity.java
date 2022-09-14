18aa3f63829959b2ab0deebb807837fc9342d324
Pavan Kumar Reddy Tadi <tadipavankumarreddy@gmail.com>
2022-09-14 07:27:27 +0530

    Notifications complete

--- Notifications/app/src/main/java/in/nareshit/notifications/MainActivity.java
+++ Notifications/app/src/main/java/in/nareshit/notifications/MainActivity.java
@@ -7,6 +7,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -15,15 +17,17 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int NOTIFICATION_ID = 456;
     public static final int PENDING_INTENT_REQ = 6;
+    private NotificationManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // To create and manage notifications we need a Notification Manager
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
     }
 
     public void sendNotification(View view) {
-        // To create and manage notifications we need a Notification Manager
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
         // TODO : Send Notification
         // TODO 1: First Create a NotificationChannel (for all the versions >= 8.0)
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
@@ -51,6 +55,16 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent pi = PendingIntent.getActivity(this,PENDING_INTENT_REQ,i,PendingIntent.FLAG_IMMUTABLE);
         builder.setContentIntent(pi);
         builder.setAutoCancel(true);
+        // Convert the current jpg image in the drawable folder to bitmap format
+        Bitmap image = BitmapFactory.decodeResource(getResources(),R.drawable.android);
+        builder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(image));
+        /*builder.setStyle(new NotificationCompat.BigTextStyle().bigText("Any large text can be written----this can be so large"));*/
+        builder.addAction(R.drawable.ic_baseline_back_hand_24,"Reply",pi);
         manager.notify(NOTIFICATION_ID,builder.build());
+    }
+
+    public void cancelNotification(View view) {
+        /*manager.cancelAll();*/
+        manager.cancel(NOTIFICATION_ID);
     }
 }
